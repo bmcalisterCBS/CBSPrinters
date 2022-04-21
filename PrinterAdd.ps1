b8891892c2a3ab1a33acbc0956d8f24042dace39
bmcalisterCBS <104154462+bmcalisterCBS@users.noreply.github.com>
2022-04-21 09:38:01 -0600

    Create PrinterAdd.ps1

---
+++ PrinterAdd.ps1
@@ -0,0 +1,7 @@
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"DEMO ROOM SAVIN IM C6000" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"DEMO ROOM Kyocera 3553ci KX" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"DEMO ROOM SAVIN IM C400SR" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"Follow me Print" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"DEMO ROOM SAVIN MP 4055" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"Sales Printer" }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\cbsazmain\"Admin Printer" }
